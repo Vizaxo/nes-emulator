bc07fb7d156406190a672db53bb8c63907d7c82e
Vizaxo <26389755+Vizaxo@users.noreply.github.com>
2024-09-03 00:58:13 +0100

    Refactor memory class and add PPU memory

--- src/Memory.h
+++ src/Memory.h
@@ -5,11 +5,6 @@
 #define RW_READ 1
 #define RW_WRITE 0
 
-namespace Memory {
-static constexpr u32 MEM_MAX = 0xffff;
-static constexpr u32 CARTRIDGE_ROM_START = 0x4020;
-}
-
 template <int n>
 struct RAM {
 	u8 memory[n];
@@ -30,7 +25,29 @@ struct ROM {
 	}
 };
 
-struct CPUMemory {
+namespace Memory {
+	static constexpr u32 MEM_MAX = 0xffff;
+	static constexpr u32 CARTRIDGE_ROM_START = 0x4020;
+};
+
+template <typename M>
+struct Mem {
+
+	struct Pinout {
+		u16 a;
+		u8 d;
+		u8 rw : 1; // read/write. If high CPU is reading
+	} pinout;
+
+	void tick() {
+		if (pinout.rw == RW_READ)
+			pinout.d = (*(M*)this)[pinout.a];
+		else
+			(*(M*)this)[pinout.a] = pinout.d;
+	}
+};
+
+struct CPUMemory : Mem<CPUMemory> {
 	RAM<0x0800> internal_ram;
 	RAM<0x08> ppu_reg;
 	RAM<0x18> apu_io_reg;
@@ -50,12 +67,6 @@ struct CPUMemory {
 			return cartridge_rom[addr - 0x4020];
 	}
 
-	struct Pinout {
-		u16 a;
-		u8 d;
-		u8 rw : 1; // read/write. If high CPU is reading
-	} pinout;
-
 	void debug_set_all_mem(u8 d) {
 		for (int i = 0; i <= Memory::MEM_MAX; ++i)
 			(*this)[i] = d;
@@ -64,11 +75,22 @@ struct CPUMemory {
 	void debug_setmem(u16 a, u8 d) {
 		(*this)[a] = d;
 	}
+};
 
-	void tick() {
-		if (pinout.rw == RW_READ)
-			pinout.d = (*this)[pinout.a];
-		else
-			(*this)[pinout.a] = pinout.d;
+struct PPUMemory : Mem<PPUMemory> {
+	ROM<0x2000> chr_rom;
+	RAM<0x0800> vram;
+	RAM<0x20> palette_ram;
+
+	u8& operator[](u16 addr) {
+		if (addr < 0x2000)
+			return chr_rom[addr];
+		if (addr < 0x3000)
+			return vram[addr % 0x800]; // TODO: mirroring modes
+		if (addr < 0x3f00)
+			return vram[addr % 0x800];
+		if (addr < 0x4000)
+			return palette_ram[addr % 0x20];
+		ASSERT(false, "PPU address %04x out of range", addr);
 	}
 };
