e20c2b7347b2b2dd2f11cce42e9ee199f9a03b29
Vizaxo <26389755+Vizaxo@users.noreply.github.com>
2024-09-03 02:34:48 +0100

    Set and clear sprite 0 hit bit to allow game to progress

--- src/PPU.h
+++ src/PPU.h
@@ -6,16 +6,16 @@
 struct PPU {
 	u64 cycles_total = 0;
 
-	u16 cycles_scanline = 0;
+	u16 dot = 0;
 	u16 scanline = 0;
 	u32 frame = 0;
 
 	void tick(cpu6502& cpu, CPUMemory& cpu_mem, PPUMemory& ppu_mem) {
 		++cycles_total;
-		++cycles_scanline;
-		if (cycles_scanline >= 341) {
+		++dot;
+		if (dot >= 341) {
 			++scanline;
-			cycles_scanline = 0;
+			dot = 0;
 		}
 		if (scanline >= 262) {
 			++frame;
@@ -23,13 +23,18 @@ struct PPU {
 		}
 
 		if (scanline == 261) {
+			if (dot == 1) {
+				cpu_mem.ppu_reg.ppustatus &= ~(1<<7 | 1<<6);
+			}
 			// pre-render scanline
 		} else if (scanline <= 239) {
+			if (scanline == 10 && dot==0)
+				cpu_mem.ppu_reg.ppustatus |= 1<<6; // fake a sprite 0 hit
 			// visible scanlines
 		} else if (scanline == 240) {
 			// post-render scanline
 		} else if (scanline >= 241 && scanline <= 260) {
-			if (scanline == 241 && cycles_scanline == 1) {
+			if (scanline == 241 && dot == 1) {
 				cpu_mem.ppu_reg.ppustatus |= 1<<7; // set vblank flag
 				if (cpu_mem.ppu_reg.ppuctrl & 1<<7)
 					cpu.pinout.nmiN = false; // send nmi if nmi-enable bit set
